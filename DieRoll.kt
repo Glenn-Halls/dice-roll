800a2e1ece48dafeed0dd5bd91c53a585d331a47
Glenn Halls <gjhalls@gmail.com>
2022-08-09 20:29:17 +1000

    Working: x sided die rolls x

--- DieRoll.kt
+++ DieRoll.kt
@@ -1,5 +1,13 @@
 fun main () {
-    val dieRange = 1..6
-    val randomNumber = dieRange.random()
-    print("Your random number is: ${randomNumber}")
+    val myFirstDie = Die()
+    val dieRoll = myFirstDie.roll()
+    println("Your ${myFirstDie.sides} sided die rolled a ${dieRoll}")
+}
+
+class Die {
+    var sides = 6
+    fun roll(): Int {
+        val randomNumber = (1..6).random()
+        return randomNumber
+    }
 }
