0723919e63245675771382b2311efd29e7e2b12b
Glenn Halls <gjhalls@gmail.com>
2022-08-10 16:52:37 +1000

    Doc: code notation

--- app/src/main/java/com/example/coinsndice/MainActivity.kt
+++ app/src/main/java/com/example/coinsndice/MainActivity.kt
@@ -13,8 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find button and set listener
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDie() }
+
+        // Do a random die roll on app startup
+        rollDie()
     }
 
     // Creates a 6 sided die and rolls it
@@ -22,16 +26,25 @@ class MainActivity : AppCompatActivity() {
         val die = Die(6)
         val dieRoll = die.roll()
 
+        // Find ImageView in layout
         val dieImage: ImageView = findViewById(R.id.imageView)
-
-        when (dieRoll) {
-            1 -> dieImage.setImageResource(R.drawable.dice_1)
-            2 -> dieImage.setImageResource(R.drawable.dice_2)
-            3 -> dieImage.setImageResource(R.drawable.dice_3)
-            4 -> dieImage.setImageResource(R.drawable.dice_4)
-            5 -> dieImage.setImageResource(R.drawable.dice_5)
-            6 -> dieImage.setImageResource(R.drawable.dice_6)
+        
+        // Determine drawable based on die roll
+        val drawableResource = when (dieRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        // Update image with correct drawable image
+        dieImage.setImageResource(drawableResource)
+
+        // Update content description with number on die
+        dieImage.contentDescription = dieRoll.toString()
+
     }
 }
 
