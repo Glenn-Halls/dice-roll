e3a0567ec95293838c414016956123d8e0f030c2
Glenn Halls <gjhalls@gmail.com>
2022-08-10 12:32:18 +1000

    Feat: random logic roll funct

--- app/src/main/java/com/example/coinsndice/MainActivity.kt
+++ app/src/main/java/com/example/coinsndice/MainActivity.kt
@@ -6,16 +6,29 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+// This activity allows the user to roll a 6 sided die and view the result
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Die Rolled!", Toast.LENGTH_SHORT).show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "5"
-        }
+        rollButton.setOnClickListener { rollDie() }
+    }
+
+    // Creates a 6 sided die and rolls it
+    private fun rollDie() {
+        val die = Die(6)
+        val dieRoll = die.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = dieRoll.toString()
+    }
+
+}
+
+// Creates a Die with [numSides] sides
+class Die(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
